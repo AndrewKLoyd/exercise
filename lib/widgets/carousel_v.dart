95aab4c1b41380b25f4f6ed806c3dd73ebff25a2
AndrewKLoyd <AndrewKLoyd@yandex.ru>
2023-08-01 16:48:59 +0300

    fixed carousel

--- lib/widgets/carousel_v.dart
+++ lib/widgets/carousel_v.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:exercise/widgets/carousel_item_v.dart';
 import 'package:flutter/material.dart';
 
@@ -7,13 +8,21 @@ class CarouselV extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.all(20),
-      height: MediaQuery.of(context).size.height * 0.2,
-      child: ListView(
-        //scrollDirection: Axis.horizontal,
-        children: carouselItems,
-      ),
+    return CarouselSlider(
+      options: CarouselOptions(height: 300),
+      items: carouselItems,
     );
   }
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Container(
+  //     margin: const EdgeInsets.all(20),
+  //     height: MediaQuery.of(context).size.height * 0.2,
+  //     child: ListView(
+  //       //scrollDirection: Axis.horizontal,
+  //       children: carouselItems,
+  //     ),
+  //   );
+  // }
 }
